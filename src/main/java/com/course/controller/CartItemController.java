020c6d7729fff8bc6ca7ff7b5de100a3bc4e7b47
Minho Kang <busster@naver.com>
2026-04-21 02:32:43 +0900

    컨트롤러 정리 및 생성

---
+++ src/main/java/com/course/controller/CartItemController.java
@@ -0,0 +1,15 @@
+package com.course.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CartItemController {
+
+    @GetMapping("/api/text")
+    public String asdf(){
+
+        return "sdf";
+    }
+}
---
+++ src/main/java/com/course/controller/LectureController.java
@@ -0,0 +1,19 @@
+package com.course.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/lecture")
+@Slf4j
+public class LectureController {
+
+    @GetMapping
+    public String getLectureList(){
+
+        return "sdf";
+    }
+}
