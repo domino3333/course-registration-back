0c8633e3c81295255801c311aac113ad9e108522
MINO\User <busster@naver.com>
2026-04-25 09:45:24 +0900

    filterChain, passwordEncoder 추가

--- src/main/java/com/course/controller/CartItemController.java
+++ src/main/java/com/course/controller/CartItemController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.course.service.CartItemService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/cartItem")
+@RequiredArgsConstructor
 public class CartItemController {
 
     private final CartItemService cartItemService;
--- src/main/java/com/course/security/SecurityConfig.java
+++ src/main/java/com/course/security/SecurityConfig.java
@@ -1,9 +1,33 @@
 package com.course.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    //TODO 시큐리티
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(formLogin->formLogin.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .sessionManagement(session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
